5e67e879ebfea0c7ff6bee1239b8d51ba413af46
unknown <benoit.templier@gmail.com>
2014-05-14 00:04:57 +0200

    finish before tag

--- www/dev/js/app.js
+++ www/dev/js/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $("nav a.trigger").click(function() {
-    $(".menu").show();
+    $("#menu").toggleClass('show');
   });
-  $(".menu a, .play, .rooms, header, .bravo").click(function() {
-    $(".menu").hide();
+  $("#menu a, .play, .rooms, header, .bravo, div.dotations").click(function() {
+    $("#menu").removeClass('show');
   });
 });
