3b52323f90845d6f616b0b16e416ea908ba2a1dc
unknown <benoit.templier@gmail.com>
2014-06-26 00:36:41 +0200

    ajout highlands

---
+++ highlands/dev/js/google-map.js
@@ -0,0 +1,72 @@
+window.onload = function() {
+    // Solvac
+    var myLatlng = new google.maps.LatLng(48.8683359,2.3243814);
+    
+    // Carte centrée sur Solvac
+    var myMapOptions = {
+        zoom: 15,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [ 
+        {
+            "stylers": [
+              { "hue": "#003bff" },
+              { "invert_lightness": true },
+              { "lightness": 10 },
+              { "gamma": 0.91 },
+              { "saturation": -60 }
+            ]
+        },{
+            "featureType": "road.local",
+            "elementType": "labels",
+            "stylers": [
+              { "visibility": "off" }
+            ]
+        },{
+            "featureType": "landscape",
+            "stylers": [
+              { "visibility": "simplified" }
+            ]
+        },{
+            "featureType": "poi",
+            "stylers": [
+              { "visibility": "off" }
+            ]
+        },{
+            "featureType": "road",
+            "stylers": [
+              { "saturation": -65 }
+            ]
+        },{
+            "featureType": "water",
+            "stylers": [
+              { "saturation": -50 },
+              { "lightness": -25 }
+            ]
+        },{
+            "featureType": "road",
+            "stylers": [
+              { "gamma": 0.9 }
+            ]
+          } 
+        ]
+    };
+    
+    // Création de la carte
+    var myMap = new google.maps.Map(
+        document.getElementById('map'),
+        myMapOptions
+    );
+    
+    // Création de l'icône
+    var myMarkerImage = new google.maps.MarkerImage('images/logo-map.png');
+    
+    // Création du marker
+    var myMarker = new google.maps.Marker({
+        position: myLatlng, 
+        map: myMap,
+        icon: myMarkerImage,
+        title: "Solvac"
+    });
+    
+};
